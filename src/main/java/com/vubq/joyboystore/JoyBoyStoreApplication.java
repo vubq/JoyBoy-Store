192d096a4766dfcf4baf7d0243957759230564c9
Quang Vũ <vubq242002@gmail.com>
2024-07-13 08:54:45 +0700

    Init project backend

---
+++ src/main/java/com/vubq/joyboystore/JoyBoyStoreApplication.java
@@ -0,0 +1,13 @@
+package com.vubq.joyboystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyBoyStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoyBoyStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vubq/joyboystore/JoyBoyStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vubq.joyboystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyBoyStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
